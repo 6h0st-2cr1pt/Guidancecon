70e6f49ad84e67c2f5c15783d23df14829c1b205
Cipher <jonaldsabordo@gmail.com>
2025-11-12 15:53:44 +0800

    v1.2.5 build.sh

---
+++ build.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+# Build script for Render deployment
+set -o errexit  # Exit on error
+
+echo "Installing dependencies..."
+pip install -r requirements.txt
+
+echo "Running migrations..."
+python manage.py migrate --noinput
+
+echo "Collecting static files..."
+python manage.py collectstatic --noinput
+
+echo "Build complete!"
+
