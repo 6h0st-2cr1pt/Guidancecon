ea41d1cfca1dded07a13cc28f879973fbdedd0e3
Cipher <jonaldsabordo@gmail.com>
2025-12-14 19:18:13 +0800

    Fix MultipleObjectsReturned error in authentication backends - handle duplicate emails/usernames

--- myproject/public/backends.py
+++ myproject/public/backends.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.backends import ModelBackend
 from django.contrib.auth.models import User
+from django.core.exceptions import MultipleObjectsReturned
 from .models import UserProfile
 
 class StudentIDBackend(ModelBackend):
@@ -7,23 +8,42 @@ class StudentIDBackend(ModelBackend):
     Custom authentication backend that allows students to login with their student ID.
     """
     def authenticate(self, request, username=None, password=None, **kwargs):
+        user = None
+        
+        # First try to find user by student ID
         try:
-            # First try to find user by student ID
             profile = UserProfile.objects.get(student_id=username)
             user = profile.user
         except UserProfile.DoesNotExist:
+            pass
+        except MultipleObjectsReturned:
+            # If multiple profiles have the same student_id, get the first one
+            profile = UserProfile.objects.filter(student_id=username).first()
+            if profile:
+                user = profile.user
+        
+        # If not found by student ID, try email
+        if user is None:
             try:
-                # Fallback to email
                 user = User.objects.get(email=username)
             except User.DoesNotExist:
-                try:
-                    # Final fallback to username
-                    user = User.objects.get(username=username)
-                except User.DoesNotExist:
-                    return None
+                pass
+            except MultipleObjectsReturned:
+                # If multiple users have the same email, prefer non-staff users (students)
+                user = User.objects.filter(email=username).order_by('is_staff', 'id').first()
+        
+        # If still not found, try username
+        if user is None:
+            try:
+                user = User.objects.get(username=username)
+            except User.DoesNotExist:
+                return None
+            except MultipleObjectsReturned:
+                # If multiple users have the same username (shouldn't happen, but handle it)
+                user = User.objects.filter(username=username).order_by('is_staff', 'id').first()
         
         # Check password
-        if user.check_password(password) and self.user_can_authenticate(user):
+        if user and user.check_password(password) and self.user_can_authenticate(user):
             return user
         return None
 
--- myproject/sysadmin/backends.py
+++ myproject/sysadmin/backends.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.backends import ModelBackend
 from django.contrib.auth.models import User
+from django.core.exceptions import MultipleObjectsReturned
 
 
 class EmailBackend(ModelBackend):
@@ -7,18 +8,29 @@ class EmailBackend(ModelBackend):
     Custom authentication backend that allows users to login with their email address.
     """
     def authenticate(self, request, username=None, password=None, **kwargs):
+        user = None
+        
+        # Try to find user by email first
         try:
-            # Try to find user by email first
             user = User.objects.get(email=username)
         except User.DoesNotExist:
+            pass
+        except MultipleObjectsReturned:
+            # If multiple users have the same email, get the first one (prefer staff users)
+            user = User.objects.filter(email=username).order_by('-is_staff', '-is_superuser', 'id').first()
+        
+        # If not found by email, try username
+        if user is None:
             try:
-                # Fallback to username
                 user = User.objects.get(username=username)
             except User.DoesNotExist:
                 return None
+            except MultipleObjectsReturned:
+                # If multiple users have the same username (shouldn't happen, but handle it)
+                user = User.objects.filter(username=username).order_by('-is_staff', '-is_superuser', 'id').first()
         
         # Check password
-        if user.check_password(password) and self.user_can_authenticate(user):
+        if user and user.check_password(password) and self.user_can_authenticate(user):
             return user
         return None
 
